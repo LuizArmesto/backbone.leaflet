4a433fee88ee17966d3abb3ccdabfc377967c2ab
Luiz Armesto <luiz.armesto@gmail.com>
2013-02-10 03:53:37 -0200

    starting the basic javascript structure

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Backbone.Leaflet (0.0.1-dev)
 
 [![Continuous Integration status](https://secure.travis-ci.org/LuizArmesto/backbone.leaflet.png)](http://travis-ci.org/LuizArmesto/backbone.leaflet)
+
+*This project is not usable yet!*
--- dist/backbone.leaflet.js
+++ dist/backbone.leaflet.js
@@ -2,6 +2,56 @@
 * http://github.com/LuizArmesto/backbone.leaflet
 * Copyright (c) 2013 Luiz Armesto; Licensed MIT */
 
-Backbone.Leaflet = (function ( Backbone, _, $ ) {
+(function ( Backbone, /* Underscore */ _, /* jQuery */ $, /* Leaflet */ L ) {
+  "use strict";
 
-}( Backbone, _, jQuery ));
+  // The top-level namespace. All public classes will be attached to this.
+  var Leaflet = {};
+
+  // Current version of the component. Keep in sync with `package.json`.
+  Leaflet.VERSION = '0.0.1-dev';
+
+  // Save the previous value of the `Leaflet` attribute.
+  var previousBackboneLeaflet = Backbone.Leaflet;
+
+  // Attach our namespace to `Backbone` variable.
+  Backbone.Leaflet = Leaflet;
+
+  // Runs Backbone.Leaflet in *noConflict* mode, returning the `Backbone`
+  // `Leaflet` attribute to its previous owner. Returns a reference to our
+  // object.
+  Leaflet.noConflict = function () {
+    Backbone.Leaflet = previousBackboneLeaflet;
+    return this;
+  };
+
+
+  // Backbone.Leaflet.GeoModel
+  // -------------------------
+
+  // Extend Backbone.Model, adding georef support.
+  var GeoModel = Leaflet.GeoModel = Backbone.Model.extend({
+
+  });
+
+
+  // Backbone.Leaflet.GeoCollection
+  // ------------------------------
+
+  // Extend Backbone.Collectio, adding georef support.
+  var GeoCollection = Leaflet.GeoCollection = Backbone.Collection.extend({
+
+  });
+
+
+  // Backbone.Leaflet.MapView
+  // ------------------------
+
+  // Backbone view to display `Backbone.Leaflet.GeoModel` and
+  // `Backbone.Leaflet.GeoCollection` instances on map.
+  var MapView = Leaflet.MapView = Backbone.View.extend({
+
+  });
+
+
+}( Backbone, _, jQuery, L ));
