371f3fefcdcdfc8c06047ec753fe37a0eff48595
Luiz Armesto <luiz.armesto@gmail.com>
2013-02-12 20:12:21 -0200

    reorganizing tests

--- test/backbone.leaflet.geocollection.test.js
+++ test/backbone.leaflet.geocollection.test.js
@@ -2,7 +2,9 @@
   describe: true, expect: true, sinon: true, it: true,
   beforeEach: true, afterEach: true, window: true*/
 
-describe( 'Backbone.Leaflet.GeoCollection', function () {
+// GeoCollection tests
+// -------------------
+describe( 'GeoCollection', function () {
 
   beforeEach( function () {
     // Create new `GeoCollection` instance before each test.
@@ -14,67 +16,91 @@ describe( 'Backbone.Leaflet.GeoCollection', function () {
     this.geoCollection = null;
   });
 
-  it( 'should allow inheritance', function () {
-    var GeoCollection = Backbone.Leaflet.GeoCollection;
-    expect( GeoCollection.extend ).to.be.equal( Backbone.Collection.extend );
-    // Should not override `initialize` function.
-    expect( GeoCollection.prototype.initialize ).to.be.equal( Backbone.Collection.prototype.initialize );
-  });
+  // Constructor tests
+  // -----------------
+  describe( 'Constructor', function () {
 
-  it( 'should have `layer` attribute', function () {
-    expect( this.geoCollection ).to.have.property( 'layer' );
-    expect( this.geoCollection.layer ).to.be.instanceOf( L.GeoJSON );
-  });
-
-  it( 'should not filter models by default', function () {
-    var modelFake = {};
-    expect( this.geoCollection.modelFilter( modelFake ) ).to.be.equal( true );
-  });
-
-  it( 'should accept `filter` option', function () {
-    var modelFake = {};
-    var geoCollection = new Backbone.Leaflet.GeoCollection( [], {
-      filter: function ( model ) {
-        // Should receive the `modelFilter` arguments.
-        expect( model ).to.be.equal( modelFake );
-        // Should be binded.
-        expect( this ).to.be.equal( geoCollection );
-        return false;
-      }
+    it( 'should allow inheritance', function () {
+      var GeoCollection = Backbone.Leaflet.GeoCollection;
+      expect( GeoCollection.extend ).to.be.equal( Backbone.Collection.extend );
+      // Should not override `initialize` function.
+      expect( GeoCollection.prototype.initialize ).to.be.equal( Backbone.Collection.prototype.initialize );
     });
-    // Should returns the return from `filter`function defined above.
-    expect( geoCollection.modelFilter( modelFake ) ).to.be.equal( false );
+
+    it( 'should initialize `layer` property', function () {
+      expect( this.geoCollection ).to.have.property( 'layer' );
+      expect( this.geoCollection.layer ).to.be.instanceOf( L.GeoJSON );
+    });
+
   });
 
-  it( 'should use `defaultStyle` as fallback to `modelStyle` function', function () {
-    var modelFake = {};
-    expect( this.geoCollection.modelStyle( modelFake ) ).to.be.equal( this.geoCollection.defaultStyle );
+  // Map elements filter tests
+  // -------------------------
+  describe( 'modelFilter', function () {
+
+    it( 'should not filter models by default', function () {
+      var modelFake = {};
+      expect( this.geoCollection.modelFilter( modelFake ) ).to.be.equal( true );
+    });
+
+    it( 'should accept `filter` option', function () {
+      var modelFake = {};
+      var geoCollection = new Backbone.Leaflet.GeoCollection( [], {
+        filter: function ( model ) {
+          // Should receive the `modelFilter` arguments.
+          expect( model ).to.be.equal( modelFake );
+          // Should be binded.
+          expect( this ).to.be.equal( geoCollection );
+          return false;
+        }
+      });
+      // Should returns the return from `filter`function defined above.
+      expect( geoCollection.modelFilter( modelFake ) ).to.be.equal( false );
+    });
+
   });
 
-  it( 'should get style from  models `getStyle` function if exists', function () {
-    var myStyle = { color: '#fff' };
-    var modelFake = {
-      getStyle: function () {
-        return myStyle;
-      }
-    };
-    expect( this.geoCollection.modelStyle( modelFake ) ).to.be.equal( myStyle );
+  // Map elements style tests
+  // ------------------------
+  describe( 'modelStyle', function () {
+
+    it( 'should use `defaultStyle` as fallback to `modelStyle` function', function () {
+      var modelFake = {};
+      expect( this.geoCollection.modelStyle( modelFake ) ).to.be.equal( this.geoCollection.defaultStyle );
+    });
+
+    it( 'should get style from  models `getStyle` function if exists', function () {
+      var myStyle = { color: '#fff' };
+      var modelFake = {
+        getStyle: function () {
+          return myStyle;
+        }
+      };
+      expect( this.geoCollection.modelStyle( modelFake ) ).to.be.equal( myStyle );
+    });
+
   });
 
-  it( 'should have `toJSON` returning GeoJSON', function () {
-    var featureGeoJSON = {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [102.0, 0.5]
-      },
-      properties: {}
-    };
-    var model = new Backbone.Leaflet.GeoModel( featureGeoJSON );
-    var geoCollection = new Backbone.Leaflet.GeoCollection( [model] );
-    expect( geoCollection.toJSON() ).to.be.deep.equal( {
-      type: 'FeatureCollection',
-      features: [featureGeoJSON]
+  // Backbone functions tests
+  // ------------------------
+  describe( 'toJSON', function () {
+
+    it( 'should return GeoJSON', function () {
+      var featureGeoJSON = {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [102.0, 0.5]
+        },
+        properties: {}
+      };
+      var model = new Backbone.Leaflet.GeoModel( featureGeoJSON );
+      var geoCollection = new Backbone.Leaflet.GeoCollection( [model] );
+      expect( geoCollection.toJSON() ).to.be.deep.equal( {
+        type: 'FeatureCollection',
+        features: [featureGeoJSON]
+      });
+
     });
 
   });
--- test/backbone.leaflet.mapview.test.js
+++ test/backbone.leaflet.mapview.test.js
@@ -2,7 +2,9 @@
   describe: true, expect: true, sinon: true, it: true,
   beforeEach: true, afterEach: true*/
 
-describe( 'Backbone.Leaflet.MapView', function () {
+// MapView tests
+// -------------
+describe( 'MapView', function () {
 
   beforeEach( function () {
     // Create new `MapView` instance before each test.
@@ -15,55 +17,67 @@ describe( 'Backbone.Leaflet.MapView', function () {
     this.mapView = null;
   });
 
-  it( 'should allow inheritance', function () {
-    var MapView = Backbone.Leaflet.MapView;
-    expect( MapView.extend ).to.be.equal( Backbone.View.extend );
-    // Should not override `initialize` function.
-    expect( MapView.prototype.initialize ).to.be.equal( Backbone.View.prototype.initialize );
-  });
+  // Constructor tests
+  // -----------------
+  describe( 'Constructor', function () {
 
-  it( 'should have `map` attribute', function () {
-    expect( this.mapView ).to.have.property( 'map' );
-    expect( this.mapView.map ).to.be.instanceOf( L.Map );
-  });
-
-  it( 'should append `Leaflet` DOM elements', function () {
-    expect( this.mapView.$el.hasClass( 'leaflet-container' ) ).to.be.equal(true);
-    expect( this.mapView.$el.html() ).to.have.string( 'leaflet-map-pane' );
-  });
-
-  it( 'should provide a `L.TileLayer` instance', function () {
-    // Should have a method to create a `L.TileLayer` instance.
-    expect( this.mapView.getTileLayer() ).to.be.instanceOf( L.TileLayer );
-    // Should initialize the `tileLayer` property.
-    expect( this.mapView ).to.have.property( 'tileLayer' );
-    expect( this.mapView.tileLayer ).to.be.instanceOf( L.TileLayer );
-  });
-
-  it( 'should delegate map events', function ( done ) {
-    var n = 0;
-    // Extend `MapView` to handle events.
-    var MyMapView = Backbone.Leaflet.MapView.extend({
-      events: {
-        'click map': 'onEvent',
-        'move map': 'onEvent'
-      },
-      onEvent: function ( e ) {
-        expect( e ).to.have.property( 'type' );
-        expect( e ).to.have.property( 'target' );
-        expect( e.target ).to.be.equal( this.map );
-        if ( ++n === 2 ) {
-          done();
-        }
-      }
+    it( 'should allow inheritance', function () {
+      var MapView = Backbone.Leaflet.MapView;
+      expect( MapView.extend ).to.be.equal( Backbone.View.extend );
+      // Should not override `initialize` function.
+      expect( MapView.prototype.initialize ).to.be.equal( Backbone.View.prototype.initialize );
     });
-    // Instanciate MyMapView
-    var myMapView = new MyMapView();
-    // Fire map events
-    myMapView.map.fire( 'click' );
-    myMapView.map.fire( 'move' );
-    // Clear `MyMapView` instance.
-    myMapView.remove();
+
+    it( 'should initialize a `map` property', function () {
+      expect( this.mapView ).to.have.property( 'map' );
+      expect( this.mapView.map ).to.be.instanceOf( L.Map );
+    });
+
+    it( 'should append `Leaflet` DOM elements', function () {
+      expect( this.mapView.$el.hasClass( 'leaflet-container' ) ).to.be.equal(true);
+      expect( this.mapView.$el.html() ).to.have.string( 'leaflet-map-pane' );
+    });
+
+    it( 'should initialize a `L.TileLayer` instance', function () {
+      // Should have a method to create a `L.TileLayer` instance.
+      expect( this.mapView.getTileLayer() ).to.be.instanceOf( L.TileLayer );
+      // Should initialize the `tileLayer` property.
+      expect( this.mapView ).to.have.property( 'tileLayer' );
+      expect( this.mapView.tileLayer ).to.be.instanceOf( L.TileLayer );
+    });
+
+  });
+
+  // Events tests
+  // ------------
+  describe( 'Events', function () {
+
+    it( 'should delegate map events', function ( done ) {
+      var n = 0;
+      // Extend `MapView` to handle events.
+      var MyMapView = Backbone.Leaflet.MapView.extend({
+        events: {
+          'click map': 'onEvent',
+          'move map': 'onEvent'
+        },
+        onEvent: function ( e ) {
+          expect( e ).to.have.property( 'type' );
+          expect( e ).to.have.property( 'target' );
+          expect( e.target ).to.be.equal( this.map );
+          if ( ++n === 2 ) {
+            done();
+          }
+        }
+      });
+      // Instanciate MyMapView
+      var myMapView = new MyMapView();
+      // Fire map events
+      myMapView.map.fire( 'click' );
+      myMapView.map.fire( 'move' );
+      // Clear `MyMapView` instance.
+      myMapView.remove();
+    });
+
   });
 
 });
